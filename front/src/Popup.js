dda1d2ce4a65201d2cc3e70781bb76c44dff7652
karu-rress <nsun5274@gmail.com>
2024-01-07 22:45:21 +0900

    front - popup window

---
+++ front/src/Popup.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ *  Popup.js
+ *  Popup Screen of Detailed info about Restaurant
+ * 
+ *  Created: 2024-01-07
+ *  Last modified: -
+ * 
+ */
+
+import React, { useEffect, useState } from 'react';
+import './Popup.css';
+
+function Popup() {
+    return (
+
+    );
+}
+
+export default Popup;
