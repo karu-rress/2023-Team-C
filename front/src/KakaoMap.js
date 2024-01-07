afa74f23bd2289bb06e6b30881aee61bda78fd94
karu-rress <nsun5274@gmail.com>
2024-01-07 21:12:15 +0900

    what is the problem?

--- front/src/KakaoMap.js
+++ front/src/KakaoMap.js
@@ -12,7 +12,7 @@ import React, { useEffect, useState } from 'react';
 import './KakaoMap.css';
 
 const kakao_map = window.kakao.maps;
-const api_address = 'http://20.205.239.240:8080';
+const api_address = 'http://localhost:8080';
 
 // 처음 지도가 표시될 때의 위치
 const initPos = new kakao_map.LatLng(37.5051, 126.9571);
